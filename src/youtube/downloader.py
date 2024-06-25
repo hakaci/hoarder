2a755ebf161353b029858ec00c9e9c29d955fca7
hakaci <hakan.dervisoglu@protonmail.com>
2024-06-25 17:03:33 +0300

    filter by channel

--- src/youtube/downloader.py
+++ src/youtube/downloader.py
@@ -54,66 +54,72 @@ def choose_channel(channel_names):
     
 
 def download_youtube_video(item_count_to_download):
+    filtered_rows = []
+
     # Get all rows from csv
     rows = get_metadata_csv_list(csv_file_path)
 
     # Create a list to hold updated rows
-    updated_rows = list(rows)
+    rows = list(rows)
 
     # Get channel names
-    channel_names = get_channels_list_from_csv(updated_rows)
+    channel_names = get_channels_list_from_csv(rows)
 
-    choosen_channel_name = choose_channel(channel_names)
+    # Get channel name from user
+    chosen_channel_name = choose_channel(channel_names)
 
-    print(choosen_channel_name)
+    # Filter by channel name
+    for row in rows[1:]:
+        if row[2] == chosen_channel_name:
+            filtered_rows.append(row)
 
-    # # Get x number of False download status rows
-    # filtered_rows = get_false_download_status_rows(rows, item_count_to_download)
+    # Get x number of False download status rows
+    filtered_rows = get_false_download_status_rows(filtered_rows, item_count_to_download)
 
-    # for filtered_row in filtered_rows:
-    #     video_id = filtered_row[0]
-    #     video_format_id = filtered_row[8]
+    for filtered_row in filtered_rows:
+        video_id = filtered_row[0]
+        video_format_id = filtered_row[8]
 
-    #     # Define the options for yt-dlp
-    #     ydl_opts = {
-    #         'format': f'{video_format_id}+bestaudio[ext=m4a]/bestaudio',  # Specify the format ID and audio codec
-    #         'outtmpl': join(output_dir, f'%(timestamp)s - %(title)s - %(channel)s.%(ext)s'),  # Output template for downloaded file
-    #         # 'merge_output_format': 'mp4',
-    #         # 'postprocessors': [{
-    #         #     'key': 'FFmpegVideoConvertor',
-    #         #     'preferedformat': 'mp4'
-    #         # }]
-    #     }
+        # Define the options for yt-dlp
+        ydl_opts = {
+            'format': f'{video_format_id}+bestaudio[ext=m4a]/bestaudio',  # Specify the format ID and audio codec
+            'outtmpl': join(output_dir, f'%(timestamp)s - %(title)s - %(channel)s.%(ext)s'),  # Output template for downloaded file
+            # 'merge_output_format': 'mp4',
+            # 'postprocessors': [{
+            #     'key': 'FFmpegVideoConvertor',
+            #     'preferedformat': 'mp4'
+            # }]
+        }
 
-    #     # Initialize YoutubeDL object
-    #     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    #         try:
-    #             # Construct the video URL using provided options
-    #             video_url = f"https://www.youtube.com/watch?v={video_id}"
+        # Initialize YoutubeDL object
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            try:
+                # Construct the video URL using provided options
+                video_url = f"https://www.youtube.com/watch?v={video_id}"
                 
-    #             # Download the video using the URL
-    #             ydl.download([video_url])
-    #             print(f"\nSuccessfully downloaded video with ID: {video_id}\n")
-    #             # Iterate over each row in CSV rows
-    #             for index, row in enumerate(rows):
-    #                 if row[0] == video_id:
-    #                     # Found the matching ID, mark download_status as True
-    #                     updated_rows[index][7] = True
-    #         except Exception as e:
-    #             print(f"\nFailed to download video with ID: {row[0]}. Error: {e}\n")
+                # Download the video using the URL
+                ydl.download([video_url])
+                print(f"\nSuccessfully downloaded video with ID: {video_id}\n")
+                # Iterate over each row in CSV rows
+                for index, row in enumerate(rows):
+                    if row[0] == video_id:
+                        # Found the matching ID, mark download_status as True
+                        rows[index][7] = True
+            except Exception as e:
+                print(f"\nFailed to download video with ID: {row[0]}. Error: {e}\n")
         
-    # header = updated_rows[0]  # First element is the header
-    # rows = updated_rows[1:]   # Remaining elements are rows
+    header = rows[0]  # First element is the header
+    rows = rows[1:]   # Remaining elements are rows
     
-    # # Write updated_rows to the CSV file
-    # with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
-    #     writer = csv.writer(file)
+    # Write updated_rows to the CSV file
+    with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
         
-    #     # Write the header
-    #     writer.writerow(header)
+        # Write the header
+        writer.writerow(header)
         
-    #     # Write all rows
-    #     writer.writerows(rows)
+        # Write all rows
+        writer.writerows(rows)
 
 
 def main():
