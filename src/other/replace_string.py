5642929fe44020e5bb606904891b1bde30c19f0b
hakaci <hakan.dervisoglu@protonmail.com>
2024-07-26 10:47:26 +0300

    multiple string from input

--- src/other/replace_string.py
+++ src/other/replace_string.py
@@ -6,11 +6,14 @@ extentions_to_get = [".mp4", ".webm", ".mp3", ".m4a"]
 
 
 def get_user_input():
-    string_to_replace = input("Enter string to replace: ")
+    strings_to_replace = input("Enter strings to replace separated by commas: ").split(',')
     replacement_string = input("Enter replacement string: ")
     folder_paths = input("Enter folder paths separated by commas: ").split(',')
-    folder_paths = [path.strip() for path in folder_paths]  # Remove any leading/trailing whitespace
-    return string_to_replace, replacement_string, folder_paths
+    
+    # Remove any leading/trailing whitespace
+    folder_paths = [path.strip() for path in folder_paths]  
+    
+    return strings_to_replace, replacement_string, folder_paths
 
 
 def replace_strings_in_filenames(files, string_to_replace, replacement_string):
@@ -21,19 +24,20 @@ def replace_strings_in_filenames(files, string_to_replace, replacement_string):
         if string_to_replace in base_name:
             new_base_name = base_name.replace(string_to_replace, replacement_string)
             new_file_path = os.path.join(dir_name, new_base_name)
-            # os.rename(file_path, new_file_path)
+            os.rename(file_path, new_file_path)
             print(f"Renamed: {base_name} -> {new_base_name}")
 
 
 def main():
     # Get user input
-    string_to_replace, replacement_string, folder_paths = get_user_input()
+    strings_to_replace, replacement_string, folder_paths = get_user_input()
     
     # Get files absolute path
     files = file_search(folder_paths, extentions_to_get)
     
     # Replace string with given string input
-    replace_strings_in_filenames(files, string_to_replace, replacement_string)
+    for string_to_replace in strings_to_replace:
+        replace_strings_in_filenames(files, string_to_replace, replacement_string)
 
 
 if __name__ == "__main__":
